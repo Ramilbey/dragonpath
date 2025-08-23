44cc726c59a1e1e6cab9a048c15a3bc93286577f
Ramilbey <ramilbey505711503@gmail.com>
2025-08-23 13:23:22 +0800

    create langueag changer

---
+++ src/LanguageContext.js
@@ -0,0 +1,26 @@
+// src/context/SimpleLanguageContext.js
+import React, { createContext, useContext, useState } from 'react';
+
+const SimpleLanguageContext = createContext();
+
+export const useSimpleLanguage = () => {
+  return useContext(SimpleLanguageContext);
+};
+
+export const SimpleLanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('english');
+
+  const value = {
+    language,
+    setLanguage,
+    isEnglish: language === 'english',
+    isRussian: language === 'russian', 
+    isUzbek: language === 'uzbek'
+  };
+
+  return (
+    <SimpleLanguageContext.Provider value={value}>
+      {children}
+    </SimpleLanguageContext.Provider>
+  );
+};

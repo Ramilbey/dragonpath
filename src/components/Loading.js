ac5d75201521294c18240669222ccbb30ef81f12
mrsand <ramilbey505711503@gmail.com>
2026-02-04 19:13:30 +0800

    change the style of a loading page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,31 +1,20 @@
 import React from "react";
+import "./Loading.css";
 
 export default function Loading() {
   return (
-    <div
-      className="loading-screen"
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        backgroundColor: "black",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        zIndex: 9999,
-      }}
-    >
-      <img
-        src="/assets/loadindragon.gif"
-        alt="Loading..."
-        style={{
-          width: "80%",        // takes 80% of screen width
-          maxWidth: "300px",   // but never bigger than 300px
-          height: "auto",      // keeps aspect ratio
-        }}
-      />
+    <div className="loading-screen">
+      <div className="loading-content">
+        <img
+          src="/assets/loadindragon.gif"
+          alt="DragonPath Loading..."
+          className="loading-gif"
+        />
+        <div className="loading-text">DragonPath</div>
+        <div className="loading-bar-container">
+          <div className="loading-bar"></div>
+        </div>
+      </div>
     </div>
   );
 }
